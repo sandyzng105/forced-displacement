b4b2604f8a54cb9b7f3c67d88db00065b7f52334
Max-ZJC <maxchenzj@gmail.com>
2025-03-01 20:42:53 -0500

    .

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_migrate import Migrate
 from database import db  
 from models import Auth  
+import os
 
 # create Flask `app`
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR}/auth.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
